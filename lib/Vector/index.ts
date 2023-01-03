738a467e8e8810bc7a14eef6b019e5b4ef09456e
Sam Anderson <sam@andomain.co.uk>
2023-01-03 05:54:09 +0000

    2022 Day 14

--- lib/Vector/index.ts
+++ lib/Vector/index.ts
@@ -1,5 +1,5 @@
 export default class Vector {
-  constructor (public x = 0, public y = 0) { }
+  constructor(public x = 0, public y = 0) { }
 
   add(vec: Vector): Vector {
     return new Vector(this.x + vec.x, this.y + vec.y);
@@ -11,6 +11,14 @@ export default class Vector {
 
   abs(): Vector {
     return new Vector(Math.abs(this.x), Math.abs(this.y));
+  }
+
+  mag(): number {
+    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+  }
+
+  eq(vec: Vector): boolean {
+    return this.x === vec.x && this.y === vec.y;
   }
 
   unit(): Vector {
