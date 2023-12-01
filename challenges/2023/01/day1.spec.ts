1a17768f38c3d003597537663191b0e3a32d9df6
Sam Anderson <sam@andomain.co.uk>
2023-12-01 07:19:00 +0000

    2023 Day 1

--- challenges/2023/01/day1.spec.ts
+++ challenges/2023/01/day1.spec.ts
@@ -1,9 +1,44 @@
-import { solution1 } from '.';
+import {
+  processPart1,
+  processPart2,
+  solution1,
+  solution2,
+} from '.';
 
-describe('Day 1', () => {
+const testInput1 = '1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet';
+const testInput2 = 'two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen';
+
+describe('2023 Day 1', () => {
   describe('Part 1', () => {
-    it('gives the expected result', () => {
-      expect(solution1()).toBe('Waiting');
+    describe('processor', () => {
+      it('combines the first and last digit', () => {
+        expect(processPart1('1abc2')).toBe(12);
+        expect(processPart1('pqr3stu8vwx')).toBe(38);
+        expect(processPart1('a1b2c3d4e5f')).toBe(15);
+        expect(processPart1('treb7uchet')).toBe(77);
+      });
+    });
+
+    it('sums the processed lines', () => {
+      expect(solution1(testInput1)).toBe(142);
+    });
+  });
+
+  describe('Part 2', () => {
+    describe('processor', () => {
+      it('combines the first and last digit or word', () => {
+        expect(processPart2('two1nine')).toBe(29);
+        expect(processPart2('eightwothree')).toBe(83);
+        expect(processPart2('abcone2threexyz')).toBe(13);
+        expect(processPart2('xtwone3four')).toBe(24);
+        expect(processPart2('4nineeightseven2')).toBe(42);
+        expect(processPart2('zoneight234')).toBe(14);
+        expect(processPart2('7pqrstsixteen')).toBe(76);
+      });
+    });
+
+    it('sums the processed lines', () => {
+      expect(solution2(testInput2)).toBe(281);
     });
   });
 });
--- challenges/2023/01/index.ts
+++ challenges/2023/01/index.ts
@@ -1,4 +1,59 @@
-// eslint-disable-next-line import/prefer-default-export
-export const solution1 = () => 'Waiting';
+import { readFileSync } from 'fs';
 
-console.log(solution1());
+const inputData = readFileSync(`${__dirname}/input.txt`).toString();
+
+// Utils
+
+const mapToDigit = (input: string) => {
+  switch (input) {
+    case 'one':
+    case 'eno': return 1;
+    case 'two':
+    case 'owt': return 2;
+    case 'three':
+    case 'eerht': return 3;
+    case 'four':
+    case 'ruof': return 4;
+    case 'five':
+    case 'evif': return 5;
+    case 'six':
+    case 'xis': return 6;
+    case 'seven':
+    case 'neves': return 7;
+    case 'eight':
+    case 'thgie': return 8;
+    case 'nine':
+    case 'enin': return 9;
+    default: return +input;
+  }
+};
+
+const reverseString = (input: string) => input.split('').reverse().join('');
+const filterDigits = (c: string) => /\d/.test(c);
+const convertToNumber = (...inputStrs: (string | number)[]) => Number(inputStrs.join(''));
+
+const processLines = (lines: string, fn: (line: string) => number) => lines
+  .split('\n')
+  .map(fn)
+  .reduce((sum, val) => sum + val, 0);
+
+// Processers
+
+export const processPart1 = (line: string) => {
+  const digits = line.split('').filter(filterDigits);
+  return convertToNumber(digits[0], digits[digits.length - 1]);
+};
+
+export const processPart2 = (line: string) => {
+  const first = line.match(/\d|(?:one|two|three|four|five|six|seven|eight|nine)/)?.[0]!;
+  const last = reverseString(line).match(/\d|(?:enin|thgie|neves|xis|evif|ruof|eerht|owt|eno)/)?.[0]!;
+
+  return convertToNumber(mapToDigit(first), mapToDigit(last));
+};
+
+// Solve
+export const solution1 = (input: string) => processLines(input, processPart1);
+export const solution2 = (input: string) => processLines(input, processPart2);
+
+console.log(`Part 1: ${solution1(inputData)}`);
+console.log(`Part 2: ${solution2(inputData)}`);
